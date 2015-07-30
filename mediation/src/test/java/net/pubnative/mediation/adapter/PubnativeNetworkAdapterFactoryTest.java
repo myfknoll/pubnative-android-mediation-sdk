61fb827e0d4f79ca03e083bd70596c90984f1da3
hari <hari@bullfin.ch>
2015-07-30 11:23:20 +0530

    Added tests for PubnativeNetworkAdapterFactory. Includes test for 4 types of adapters,and creation with null,empty and invalid inputs

--- mediation/src/test/java/net/pubnative/mediation/adapter/PubnativeNetworkAdapterFactoryTest.java
+++ mediation/src/test/java/net/pubnative/mediation/adapter/PubnativeNetworkAdapterFactoryTest.java
@@ -1,15 +1,90 @@
 package net.pubnative.mediation.adapter;
 
+import net.pubnative.mediation.adapter.network.FacebookNetworkAdapter;
+import net.pubnative.mediation.adapter.network.LoopmeNetworkAdapter;
+import net.pubnative.mediation.adapter.network.PubnativeLibraryNetworkAdapter;
+import net.pubnative.mediation.adapter.network.YahooNetworkAdapter;
+import net.pubnative.mediation.model.PubnativeNetworkModel;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * Created by davidmartin on 28/07/15.
+ */
 public class PubnativeNetworkAdapterFactoryTest
 {
+    PubnativeNetworkModel testModel;
     @Test
     public void test_empty()
     {
         assertThat(true).isTrue();
     }
-    // TODO: Create tests
+
+    @Before
+    public void setUp()
+    {
+        testModel = new PubnativeNetworkModel();
+    }
+
+    // TODO: Create test
+
+    @Test
+    public void test_pubnativeLibraryNetwork()
+    {
+        testModel.adapter = "net.pubnative.mediation.adapter.network.PubnativeLibraryNetworkAdapter";
+        PubnativeNetworkAdapter adapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(adapterInstance).isInstanceOf(PubnativeLibraryNetworkAdapter.class);
+    }
+
+    @Test
+    public void test_facebookNetwork()
+    {
+        testModel.adapter = "net.pubnative.mediation.adapter.network.FacebookNetworkAdapter";
+        PubnativeNetworkAdapter facebookNetworkAdapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(facebookNetworkAdapterInstance).isInstanceOf(FacebookNetworkAdapter.class);
+    }
+
+    @Test
+    public void test_loopMeNetwork()
+    {
+        testModel.adapter = "net.pubnative.mediation.adapter.network.LoopmeNetworkAdapter";
+        PubnativeNetworkAdapter loopMeNetworkAdapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(loopMeNetworkAdapterInstance).isInstanceOf(LoopmeNetworkAdapter.class);
+    }
+
+    @Test
+    public void test_yahooNetwork()
+    {
+        testModel.adapter = "net.pubnative.mediation.adapter.network.YahooNetworkAdapter";
+        PubnativeNetworkAdapter yahooNetworkAdapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(yahooNetworkAdapterInstance).isInstanceOf(YahooNetworkAdapter.class);
+    }
+
+    @Test
+    public void test_invalidStringInput()
+    {
+        testModel.adapter = "HelloWorld";
+        PubnativeNetworkAdapter adapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(adapterInstance).isNull();
+    }
+
+    @Test
+    public void test_nullInput()
+    {
+        testModel.adapter = null;
+        PubnativeNetworkAdapter adapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(adapterInstance).isNull();
+    }
+
+    @Test
+    public void test_emptyInput()
+    {
+        testModel.adapter = "";
+        PubnativeNetworkAdapter adapterInstance = PubnativeNetworkAdapterFactory.createAdapter(testModel);
+        assertThat(adapterInstance).isNull();
+    }
+
 }
