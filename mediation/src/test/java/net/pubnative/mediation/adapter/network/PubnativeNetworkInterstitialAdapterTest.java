7b3d4d49213c9613c9cdacbc27c76f95fb39eac7
RagnarR <rrhack@gmail.com>
2016-06-15 13:43:06 +0200

    Added tests for Interstitial Adapter class

---
+++ mediation/src/test/java/net/pubnative/mediation/adapter/network/PubnativeNetworkInterstitialAdapterTest.java
@@ -0,0 +1,9 @@
+package net.pubnative.mediation.adapter.network;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by alvarlega on 15/06/16.
+ */
+public class PubnativeNetworkInterstitialAdapterTest {
+}
